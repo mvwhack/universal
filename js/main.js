9a7c386b998b327e3fdda31c0f5f6f0005c2b4d6
Vlad <vmansurov@icloud.com>
2020-08-12 00:23:00 +0300

    create: Added tabs section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
         $("body").toggleClass("scroll-hidden");
     });
 
+    var tabsItem = $(".tabs__item");
+    var tabsContent = $(".tabs__content");
+
+    tabsItem.on("click", function (event) {
+       var activeContent = $(this).attr("data-target");
+       $(tabsItem).removeClass("tabs__item--active");
+       $(this).addClass("tabs__item--active");
+       $(tabsContent).removeClass("tabs__content--active");
+       $(activeContent).addClass("tabs__content--active");
+    });
 });
