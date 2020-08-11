80b70d081a97f2f6dbf5be13d7447d1b54bef497
Vlad <vmansurov@icloud.com>
2020-08-11 14:58:52 +0300

    create: Top-news section & responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var menuButton = $(".menu-button");
+    var menuButton = $(".navbar-top__hamburger");
     menuButton.on('click', function() {
         $(".navbar__wrapper").toggleClass('navbar__wrapper--visible');
         $("body").toggleClass("scroll-hidden");
